67d35e559dd6db0ac65da48b51a9a7c3f48eb12c
Sai Lochan Dash <128344365+smix23@users.noreply.github.com>
2023-05-03 17:37:20 +0530

    Update a2q8.java

--- a2q8.java
+++ a2q8.java
@@ -3,17 +3,49 @@ Another class Person contains a static data member maxcount,
 instance member name & method display ( ) to display name of a person,
 count the no. of characters present in the name of the person */
 
-class person
+interface DetailInfo 
 {
-    public static int maxcount;
+    void display();
+    int count();
+}
+
+class person implements DetailInfo 
+{
+    static int maxcount = 0;
     String name;
 
-    void display()
+    public person(String n) 
     {
-
+        name = n;
     }
-}
-public class a2q8 
-{
+
+    public void display() 
+    {
+        System.out.println("Name: " + name);
+    }
+
+    public int count() 
+    {
+        int length = name.length();
+        if (length > maxcount) 
+        {
+            maxcount = length;
+        }
+        return length;
+    }
     
 }
+public class a2q8
+{
+    public static void main(String[] args) 
+    {
+        person p1 = new person("Tony Stark");
+        person p2 = new person("Steven Rogers");
+        p1.display();
+        System.out.println("Count: " + p1.count());
+        p2.display();
+        System.out.println("Count: " + p2.count());
+        System.out.println("Max count: " + person.maxcount);
+    }
+}
+
